8efecba317785e2e14a4c939e379e8bd37f154f5
Antoine Ottomani <antoine.ottomani@gmail.com>
2023-01-31 09:47:01 +0100

    Étape 1 presque finie

---
+++ main.py
@@ -0,0 +1,53 @@
+# Imports
+import requests
+from bs4 import BeautifulSoup
+import csv
+
+data_book = {}
+
+
+def get_data_book(url):
+    response = requests.get(url)
+
+    if response.ok:
+        soup = BeautifulSoup(
+            response.content, 'html.parser', from_encoding="utf-8")
+
+        # Get title
+        title = soup.find("h1")
+        data_book["title"] = title.text
+
+        # Get category
+        menu_links = soup.findAll("ul", class_="breadcrumb")
+        for elt in menu_links:
+            links = elt.findAll("a")
+            category = links[-1].text
+
+        # Add category to data_book
+        data_book["category"] = category
+
+        # Get informations from table
+        tds = soup.findAll("td")
+        clean_data = []
+        for td in tds:
+            clean_data.append(td.text)
+
+        data_book["universal_product_code"] = clean_data[0]
+        data_book["price_excluding_tax"] = clean_data[2]
+        data_book["price_including_tax"] = clean_data[3]
+        data_book["number_available"] = clean_data[-2]
+
+        # Get product_description
+        description = soup.find("article").find("p", recursive=False).text
+        data_book["product_description"] = description
+
+
+# Getting data from one book
+get_data_book(
+    "http://books.toscrape.com/catalogue/sapiens-a-brief-history-of-humankind_996/index.html")
+
+# Write datas into a csv file
+with open('output.csv', "w") as csv_file:
+    writer = csv.DictWriter(csv_file, fieldnames=data_book.keys())
+    writer.writeheader()
+    writer.writerow(data_book)
